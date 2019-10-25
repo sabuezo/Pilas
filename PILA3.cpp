6241c486cc5db0ac0a873d1c20d8ce3460e868d3
Jesus Martinez Ruiz <jesusmartinezruiz24@gmail.com>
2019-10-25 12:08:58 -0500

    se ingresa caracteres a la pila y los muestra

---
+++ PILA3.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+
+struct Nodo{
+	char dato;
+	Nodo *siguiente;
+};
+
+
+void menu();
+void agregarPila(Nodo *&,char);
+void sacarPila(Nodo *&,char &);
+
+int main(){
+
+	menu();
+
+	getch();
+	return 0;
+}
+
+void menu(){
+	Nodo *pila = NULL;
+	int opcion;
+	char dato;
+
+	do{
+		cout<<"\t....:MENU PILAS:....\n";
+		cout<<"1. Ingresar un caracter a la pila"<<endl;
+		cout<<"2. Mostrar  los elementos de la pila"<<endl;
+		cout<<"3. Salir"<<endl;
+		cout<<"Opcion: ";
+		cin>>opcion;
+
+		switch(opcion){
+			case 1: cout<<"\nIngrese un caracter: ";
+					cin>>dato;
+					agregarPila(pila,dato);
+					break;
+			case 2: cout<<"\nMostrando  los elementos de la pila: ";
+					while(pila != NULL){
+						sacarPila(pila,dato);
+						if(pila != NULL){
+							cout<<dato<<" , ";
+						}
+						else{
+							cout<<dato<<".";
+						}
+					}
+					cout<<"\n";
+					system("pause");
+					break;
+			case 3: break;
+		}
+		system("cls");
+	}while(opcion != 3);
+
+}
+
+void agregarPila(Nodo *&pila,char n){
+	Nodo *nuevo_nodo = new Nodo();
+	nuevo_nodo->dato = n;
+	nuevo_nodo->siguiente = pila;
+	pila = nuevo_nodo;
+}
+
+void sacarPila(Nodo *&pila,char &n){
+	Nodo *aux = pila;
+	n = aux->dato;
+	pila = aux->siguiente;
+	delete aux;
+}
